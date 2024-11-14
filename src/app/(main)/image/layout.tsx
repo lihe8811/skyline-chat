c0770d42f37a9ed18f7173c5218470ac4a93626d
He Li <lihe8811@gmail.com>
2024-11-14 13:35:30 +0800

    remove original nav parts from image page layout

--- src/app/(main)/image/layout.tsx
+++ src/app/(main)/image/layout.tsx
@@ -5,53 +5,38 @@
 import { ChatHeader } from '@lobehub/ui';
 import { LobeHub } from '@lobehub/icons';
 import Link from 'next/link';
-import React, { ReactNode } from 'react';
 import { Flexbox } from 'react-layout-kit';
 
-export default function ImageLayout({
-  children, 
-  nav 
-}: {
-  children: ReactNode,
-  nav: ReactNode
-}) {
+export default function ImageLayout({ children }) {
   return (
     <Flexbox
       height={'100%'}
-      horizontal
       style={{ overflow: 'hidden', position: 'relative' }}
       width={'100%'}
     >
-      {nav}
+      <ChatHeader
+        left={
+          <Link href={'/image'} style={{ color: 'inherit' }}>
+            <LobeHub.Text size={32} />
+          </Link>
+        }
+        style={{
+          position: 'relative',
+          zIndex: 10,
+        }}
+        styles={{
+          center: { flex: 1, maxWidth: 1440 },
+          left: { flex: 1, maxWidth: 240 },
+          right: { flex: 1, maxWidth: 240 },
+        }}
+      />
       <Flexbox
         height={'100%'}
+        horizontal
         style={{ overflow: 'hidden', position: 'relative' }}
         width={'100%'}
       >
-        <ChatHeader
-          left={
-            <Link href={'/image'} style={{ color: 'inherit' }}>
-              <LobeHub.Text size={32} />
-            </Link>
-          }
-          style={{
-            position: 'relative',
-            zIndex: 10,
-          }}
-          styles={{
-            center: { flex: 1, maxWidth: 1440 },
-            left: { flex: 1, maxWidth: 240 },
-            right: { flex: 1, maxWidth: 240 },
-          }}
-        />
-        <Flexbox
-          height={'100%'}
-          horizontal
-          style={{ overflow: 'hidden', position: 'relative' }}
-          width={'100%'}
-        >
-          {children}
-        </Flexbox>
+        {children}
       </Flexbox>
     </Flexbox>
   );
