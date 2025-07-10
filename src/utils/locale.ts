6475e0fe1d3c9ced656f2edcb32dd2152218bc83
Arvin Xu <arvinx@foxmail.com>
2025-07-10 09:46:30 +0800

    🐛 fix: fix locale hydration error in SSR (#8365)
    * fix locale
    
    * update readme
    
    * Update README.zh-CN.md
    
    * update readme
    
    * update README
    
    * fix locales
    
    * add docs

--- src/utils/locale.ts
+++ src/utils/locale.ts
@@ -1,7 +1,8 @@
 import { resolveAcceptLanguage } from 'resolve-accept-language';
 
 import { DEFAULT_LANG } from '@/const/locale';
-import { locales, normalizeLocale } from '@/locales/resources';
+import { Locales, locales, normalizeLocale } from '@/locales/resources';
+import { RouteVariants } from '@/utils/server/routeVariants';
 
 export const getAntdLocale = async (lang?: string) => {
   let normalLang: any = normalizeLocale(lang);
@@ -45,6 +46,9 @@ export const parseBrowserLanguage = (headers: Headers, defaultLang: string = DEF
   return browserLang;
 };
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 63f482a13 (🐛 fix: fix locale hydration error in SSR (#8365))
 
 /**
  * Parse the page locale from the URL and search
@@ -59,5 +63,8 @@ export const parsePageLocale = async (props: {
   const browserLocale = await RouteVariants.getLocale(props);
   return normalizeLocale(searchParams?.hl || browserLocale) as Locales;
 };
+<<<<<<< HEAD
 =======
 >>>>>>> 416a4b121 (✨ feat: Add MCP marketplace and mcp plugin one-click installation in desktop (#8334))
+=======
+>>>>>>> 63f482a13 (🐛 fix: fix locale hydration error in SSR (#8365))
