0af8464ab50f485e35967186326302f435e50363
Arvin Xu <arvinx@foxmail.com>
2025-07-13 13:19:21 +0800

    ✨ feat: add network proxy for desktop (#7848)
    * add network proxy
    
    * update network proxy
    
    * refactor network proxy
    
    * support network proxy
    
    * fix types
    
    * fix lint
    
    * fix lint

--- apps/desktop/src/main/appBrowsers.ts
+++ apps/desktop/src/main/appBrowsers.ts
@@ -36,18 +36,6 @@ export const appBrowsers = {
     autoHideMenuBar: true,
     height: 800,
     identifier: 'settings',
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    // keepAlive: true,
-=======
->>>>>>> 46d25092a (✨ feat: add network proxy for desktop (#7848))
-=======
-    keepAlive: true,
->>>>>>> 37bb8ba2b (🔨 chore: fix settings in desktop (#8414))
-=======
-    // keepAlive: true,
->>>>>>> 28c1fe060 (⚡️ perf: improve  memory usage in desktop (#8431))
     minWidth: 600,
     parentIdentifier: 'chat',
     path: '/settings',
